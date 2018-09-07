18fd8e29e75af4b39bf6afcf1fa65d6a546d0a4a
herbo <herbo@158.37.196.209>
2018-09-07 15:18:41 +0200

    Oblig1 - oppg2

---
+++ Øvelse2/src/no/hvl/dat108/ProdusentOgKonsument.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package no.hvl.dat108;
+
+/**
+ * @author Herborg
+ *
+ */
+public class ProdusentOgKonsument {
+
+	/**
+	 * Lager eit main-program som køyrer produsenten og konsumenten 
+	 * samstundes.
+	 */
+	public static void main(String[] args) {
+		Buffer buffer = new Buffer();
+		Consumer con = new Consumer(buffer);
+		Producer pro = new Producer(buffer);
+//		Thread t1 = new Thread(con);
+//		Thread t2 = new Thread(pro);
+//		t1.start();
+//		t2.start();
+
+		con.start();
+		pro.start();
+	}
+
+}
